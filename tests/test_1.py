6b6be0c43fca2681e37108fc69c94702ad7aaca5
Clément Robert <cr52@protonmail.com>
2024-10-09 16:25:22 +0200

    cleanup

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,4 @@
 import matplotlib.pyplot as plt
-import pytest
-
 
 def test_subplots_simple():
     fig, ax = plt.subplots()
-
-@pytest.mark.mpl_image_compare()
-def test_subplots_with_pytest_mpl():
-    fig, ax = plt.subplots()
-    return fig
